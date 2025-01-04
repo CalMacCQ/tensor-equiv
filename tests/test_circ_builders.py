aaac88972c151e73d5cf837713670f718c1d5c37
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-01-04 17:11:19 +0000

    fix: remove unused code (#8)
    * Update test_circ_builders.py
    
    * Update comment

--- tests/test_circ_builders.py
+++ tests/test_circ_builders.py
@@ -28,16 +28,9 @@ def test_choi_state_circ(circ) -> None:
     )
 
 
-circ_pairs = [
-    (
-        Circuit(3).CX(0, 1).CX(1, 2).Rz(0.25, 2).CX(1, 2).CX(0, 1),
-        Circuit(3).CX(0, 2).CX(1, 2).Rz(0.25, 2).CX(1, 2).CX(0, 2),
-    )
-]
-
 n_qubit_cases = [2, 4, 7]
 
-
+# Test ancilla check construction for QFT circuits.
 @pytest.mark.parametrize("n", n_qubit_cases)
 def test_ancilla_check_circuits(n) -> None:
     test_circ = build_qft_circuit(n_qubits=n)
